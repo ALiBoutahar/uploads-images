42449d33ef37f17f8be8edd84f43cc20340ce731
ALiBoutahar <ali99boutahar@gmail.com>
2023-06-30 20:16:27 +0100

    v.2

--- routes.js
+++ routes.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer  = require('multer');
+const path = require('path');
 const app = express();
 
+let store = '../';
 const storage = multer.diskStorage({
 
   destination: function (req, file, cb) {
-    cb(null, '../images ali');
+    cb(null, store);
   },
-
+ 
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + '.png');
+    const fileExtension = path.extname(file.originalname);
+    const newFilename = uniqueSuffix + fileExtension;
+    cb(null, newFilename);
   }
+
+  // filename: function (req, file, cb) {
+  //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+  //   cb(null, uniqueSuffix + '.png');
+  // }
 });
 
 const upload = multer({ storage: storage });
@@ -20,18 +29,29 @@ const upload = multer({ storage: storage });
 app.set('view engine', 'ejs');
 app.use(express.static('images'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
-  res.render("index");
+  res.render("index",{title:'Choose Storage...'});
 });
+app.post('/store', async (req, res) => {
+  store=(req.body.store);
+  let m ;
+  if (store == '../'){m = 'Desktop'; console.log('store created Desktop');};
+  if(store == '../files ali'){m = 'files ali'; console.log('store created files ali');};
+  if(store == 'files/'){m = 'Local files'; console.log('store created Local files');};
+  res.render('index',{title:m});
+}); 
 
-app.post('/create',  upload.array('images[]'), async (req, res) => {
-  res.render('index');
+app.post('/create', upload.array('images[]'), async (req, res) => {
+  let m ;
+  if (store == '../') {m = 'Desktop'};
+  if(store == '../files ali'){m = 'files ali'};
+  if(store == 'files/'){m = 'Local files'};
+  res.render('index',{title:m});
   req.files.forEach((file) => {
     console.log(file.filename);
   });
-  console.log('images saved');
+  console.log('files saved');
 });
 
 app.listen(11235, () => {
